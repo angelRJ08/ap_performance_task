a7aaa000c63b82b5b388e9e2e18edc167e57adb0
angelRJ08 <154650658+angelRJ08@users.noreply.github.com>
2024-02-27 16:15:41 -0600

    check if correct key is pressed

--- main.ts
+++ main.ts
@@ -2,42 +2,62 @@ namespace SpriteKind {
     export const keys = SpriteKind.create()
 }
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+    pressed_correct_key(2)
 })
 scene.onOverlapTile(SpriteKind.keys, assets.tile`myTile1`, function (sprite, location) {
-    info.changeScoreBy(1)
+    info.changeScoreBy(-1)
     sprites.destroy(sprite)
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+    pressed_correct_key(1)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+    pressed_correct_key(0)
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+    pressed_correct_key(3)
 })
-function pressed_correct_key (num: number) {
-    if (true) {
-    	
+function pressed_correct_key (direction: number) {
+    if (info.score() != 0) {
+        if (direction == current_key) {
+            game.splash("good job")
+        }
     }
 }
-let Direction = 0
+let current_key = 0
 let down_key: Sprite = null
 let up_key: Sprite = null
 let left_key: Sprite = null
-let right_key: Sprite = null
+let right_arrow: Sprite = null
 tiles.setCurrentTilemap(tilemap`level1`)
-info.setLife(3)
+info.setScore(3)
 let list = [
 0,
-1,
+2,
 2,
 3
 ]
+let tiles2 = [img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `]
 for (let index = 0; index <= 3; index++) {
     if (list[index] == 0) {
-        right_key = sprites.create(img`
+        right_arrow = sprites.create(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . f f . . . . . 
             . . . . . . . . . f f . . . . . 
@@ -55,8 +75,8 @@ for (let index = 0; index <= 3; index++) {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             `, SpriteKind.keys)
-        right_key.setVelocity(0, 25)
-        right_key.setPosition(126, 29)
+        right_arrow.setVelocity(0, 25)
+        right_arrow.setPosition(126, 29)
     } else if (list[index] == 1) {
         left_key = sprites.create(img`
             . . . . . . . . . . . . . . . . 
@@ -77,7 +97,7 @@ for (let index = 0; index <= 3; index++) {
             . . . . . . . . . . . . . . . . 
             `, SpriteKind.keys)
         left_key.setVelocity(0, 25)
-        right_key.setPosition(26, 28)
+        left_key.setPosition(48, 18)
     } else if (list[index] == 2) {
         up_key = sprites.create(img`
             . . . . . . . . . . . . . . . . 
@@ -98,7 +118,7 @@ for (let index = 0; index <= 3; index++) {
             . . . . . f f f f f f . . . . . 
             `, SpriteKind.keys)
         up_key.setVelocity(0, 25)
-        up_key.setPosition(48, 18)
+        up_key.setPosition(26, 28)
     } else if (list[index] == 3) {
         down_key = sprites.create(img`
             . . . . . . . . . . . . . . . . 
@@ -121,260 +141,6 @@ for (let index = 0; index <= 3; index++) {
         down_key.setVelocity(0, 25)
         down_key.setPosition(87, 25)
     }
+    current_key = list[index]
     pause(1000)
 }
-tiles.setCurrentTilemap(tilemap`level1`)
-for (let index = 0; index < 4; index++) {
-	
-}
-tiles.setCurrentTilemap(tilemap`level1`)
-for (let index = 0; index < 4; index++) {
-	
-}
-tiles.setCurrentTilemap(tilemap`level1`)
-for (let index = 0; index < 4; index++) {
-	
-}
-forever(function () {
-    Direction = randint(0, 3)
-    if (Direction == 0) {
-        right_key = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . f f . . . . . 
-            . . . . . . . . . f f . . . . . 
-            . . . . . . . . . f f . . . . . 
-            . . . f f f f f f f f f . . . . 
-            . f f f f f f f f f f f f f . . 
-            . f f f f f f f f f f f f f f f 
-            . f f f f f f f f f f f f f f f 
-            . f f f f f f f f f f f f f f f 
-            . f f f f f f f f f f f f f . . 
-            . . . . . . . . . f f f . . . . 
-            . . . . . . . . . f f . . . . . 
-            . . . . . . . . . f f . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, SpriteKind.keys)
-    } else if (Direction == 1) {
-        left_key = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . f f . . . . . . . . . 
-            . . . . f f f . . . . . . . . . 
-            . . . f f f f . . . . . . . . . 
-            . . f f f f f f f f f f f f f . 
-            . f f f f f f f f f f f f f f . 
-            f f f f f f f f f f f f f f f . 
-            f f f f f f f f f f f f f f f . 
-            . f f f f f f f f f f f f f f . 
-            . . f f f f f f f f f f f f f . 
-            . . . f f f f . . . . . . . . . 
-            . . . . f f f . . . . . . . . . 
-            . . . . . f f . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, SpriteKind.keys)
-    } else if (Direction == 2) {
-        up_key = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . f f . . . . . . . 
-            . . . . . . f f f f . . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . f f f f f f f f f . . . . 
-            . . f f f f f f f f f f f f . . 
-            . . f f f f f f f f f f f f . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            `, SpriteKind.keys)
-    } else {
-        down_key = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . f f . . . . . 
-            . . . . . . . . . f f f . . . . 
-            . . . . . . . . . f f f f . . . 
-            . f f f f f f f f f f f f f . . 
-            . f f f f f f f f f f f f f f . 
-            . f f f f f f f f f f f f f f f 
-            . f f f f f f f f f f f f f f f 
-            . f f f f f f f f f f f f f f . 
-            . f f f f f f f f f f f f f . . 
-            . . . . . . . . . f f f f . . . 
-            . . . . . . . . . f f f . . . . 
-            . . . . . . . . . f f . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, SpriteKind.keys)
-    }
-})
-forever(function () {
-    Direction = randint(0, 3)
-    if (Direction == 0) {
-        right_key = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . f f . . . . . 
-            . . . . . . . . . f f . . . . . 
-            . . . . . . . . . f f . . . . . 
-            . . . f f f f f f f f f . . . . 
-            . f f f f f f f f f f f f f . . 
-            . f f f f f f f f f f f f f f f 
-            . f f f f f f f f f f f f f f f 
-            . f f f f f f f f f f f f f f f 
-            . f f f f f f f f f f f f f . . 
-            . . . . . . . . . f f f . . . . 
-            . . . . . . . . . f f . . . . . 
-            . . . . . . . . . f f . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, SpriteKind.keys)
-    } else if (Direction == 1) {
-        left_key = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . f f . . . . . . . . . 
-            . . . . f f f . . . . . . . . . 
-            . . . f f f f . . . . . . . . . 
-            . . f f f f f f f f f f f f f . 
-            . f f f f f f f f f f f f f f . 
-            f f f f f f f f f f f f f f f . 
-            f f f f f f f f f f f f f f f . 
-            . f f f f f f f f f f f f f f . 
-            . . f f f f f f f f f f f f f . 
-            . . . f f f f . . . . . . . . . 
-            . . . . f f f . . . . . . . . . 
-            . . . . . f f . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, SpriteKind.keys)
-    } else if (Direction == 2) {
-        up_key = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . f f . . . . . . . 
-            . . . . . . f f f f . . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . f f f f f f f f f . . . . 
-            . . f f f f f f f f f f f f . . 
-            . . f f f f f f f f f f f f . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            `, SpriteKind.keys)
-    } else {
-        down_key = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . f f . . . . . 
-            . . . . . . . . . f f f . . . . 
-            . . . . . . . . . f f f f . . . 
-            . f f f f f f f f f f f f f . . 
-            . f f f f f f f f f f f f f f . 
-            . f f f f f f f f f f f f f f f 
-            . f f f f f f f f f f f f f f f 
-            . f f f f f f f f f f f f f f . 
-            . f f f f f f f f f f f f f . . 
-            . . . . . . . . . f f f f . . . 
-            . . . . . . . . . f f f . . . . 
-            . . . . . . . . . f f . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, SpriteKind.keys)
-    }
-})
-forever(function () {
-    Direction = randint(0, 3)
-    if (Direction == 0) {
-        right_key = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . f f . . . . . 
-            . . . . . . . . . f f . . . . . 
-            . . . . . . . . . f f . . . . . 
-            . . . f f f f f f f f f . . . . 
-            . f f f f f f f f f f f f f . . 
-            . f f f f f f f f f f f f f f f 
-            . f f f f f f f f f f f f f f f 
-            . f f f f f f f f f f f f f f f 
-            . f f f f f f f f f f f f f . . 
-            . . . . . . . . . f f f . . . . 
-            . . . . . . . . . f f . . . . . 
-            . . . . . . . . . f f . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, SpriteKind.keys)
-    } else if (Direction == 1) {
-        left_key = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . f f . . . . . . . . . 
-            . . . . f f f . . . . . . . . . 
-            . . . f f f f . . . . . . . . . 
-            . . f f f f f f f f f f f f f . 
-            . f f f f f f f f f f f f f f . 
-            f f f f f f f f f f f f f f f . 
-            f f f f f f f f f f f f f f f . 
-            . f f f f f f f f f f f f f f . 
-            . . f f f f f f f f f f f f f . 
-            . . . f f f f . . . . . . . . . 
-            . . . . f f f . . . . . . . . . 
-            . . . . . f f . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, SpriteKind.keys)
-    } else if (Direction == 2) {
-        up_key = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . f f . . . . . . . 
-            . . . . . . f f f f . . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . f f f f f f f f f . . . . 
-            . . f f f f f f f f f f f f . . 
-            . . f f f f f f f f f f f f . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f f f f f . . . . . 
-            `, SpriteKind.keys)
-    } else {
-        down_key = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . f f . . . . . 
-            . . . . . . . . . f f f . . . . 
-            . . . . . . . . . f f f f . . . 
-            . f f f f f f f f f f f f f . . 
-            . f f f f f f f f f f f f f f . 
-            . f f f f f f f f f f f f f f f 
-            . f f f f f f f f f f f f f f f 
-            . f f f f f f f f f f f f f f . 
-            . f f f f f f f f f f f f f . . 
-            . . . . . . . . . f f f f . . . 
-            . . . . . . . . . f f f . . . . 
-            . . . . . . . . . f f . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, SpriteKind.keys)
-    }
-})
-forever(function () {
-	
-})

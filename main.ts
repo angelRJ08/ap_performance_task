411ba1498318a3f4291a69eed634fa46ffcb6495
angelRJ08 <154650658+angelRJ08@users.noreply.github.com>
2024-03-08 19:54:09 -0600

    noise when wrong

--- main.ts
+++ main.ts
@@ -6,17 +6,17 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     pressed_correct_key(2)
 })
 scene.onOverlapTile(SpriteKind.keys, assets.tile`myTile1`, function (sprite, location) {
-    info.changeScoreBy(-1)
-    sprites.destroy(sprite)
+    if (!(sprites.readDataBoolean(sprite, "Pressed"))) {
+        info.changeLifeBy(-1)
+        scene.cameraShake(3, 200)
+    }
+    DeleteLatestTile = tiles2.removeAt(tiles2.indexOf(sprite))
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     pressed_correct_key(1)
 })
 sprites.onDestroyed(SpriteKind.tile, function (sprite) {
-    if (!(sprites.readDataBoolean(sprite, "Pressed"))) {
-        info.setScore(info.score() - 1)
-    }
-    DeleteLatestTile = tiles2.removeAt(tiles2.indexOf(sprite))
+	
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     pressed_correct_key(0)
@@ -25,12 +25,18 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     pressed_correct_key(3)
 })
 function pressed_correct_key (direction: number) {
-    if (tiles2.length > 0 && info.score() != 0) {
+    if (tiles2.length > 0) {
         latestTile = tiles2[0]
         if (sprites.readDataNumber(latestTile, "direction") == direction) {
+            info.changeScoreBy(1)
+            sprites.destroy(latestTile, effects.fire, 200)
             sprites.setDataBoolean(latestTile, "Pressed", true)
-            sprites.destroy(latestTile, effects.fire, 500)
             DeleteLatestTile = tiles2.removeAt(0)
+        } else {
+            music.play(music.tonePlayable(262, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+            scene.cameraShake(4, 200)
+            info.changeLifeBy(-1)
+            latestTile.startEffect(effects.rings)
         }
     }
 }
@@ -39,8 +45,10 @@ let tile: Sprite = null
 let latestTile: Sprite = null
 let DeleteLatestTile: Sprite = null
 let tiles2: Sprite[] = []
+let lifeOfPlayer = true
 tiles.setCurrentTilemap(tilemap`level1`)
-info.setScore(3)
+info.setScore(5)
+info.setLife(3)
 let list = [
 0,
 1,
@@ -51,6 +59,9 @@ tiles2 = sprites.allOfKind(SpriteKind.tile)
 forever(function () {
     for (let index = 0; index <= 3; index++) {
         pause(1000)
+        if (info.life() == 0) {
+            lifeOfPlayer = false
+        }
         if (list[index] == 0) {
             tile = sprites.create(img`
                 . . . . . . . . . . . . . . . . 
